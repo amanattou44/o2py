fd461763a7b32f16cf3cc1639dbf52fe80ae4f5f
amanattou44 <amanattou44@gmail.com>
2013-06-09 08:35:39 +0900

    First commit

---
+++ py.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print " お　　　　　　　　　　　　　　　　　　　　い"
+print "つ　　　　　　　　　　　　　　　　 　 　 　 ぱ"
+print "ぱ　　　　　　 　 　 　 っ　　　　　　　　　 つ"
+print " い　　　　　　　　 　 おぱ　　　　　　　 　 お"
+print " お　　　　　::っ 　 　い　い　　　::っ　　　い"
+print " つっ　　　　　　　ぱ　　　お　　　　　　  ぱ"
+print "      つつっっつ　　　　　　つっっっつつつ" 
